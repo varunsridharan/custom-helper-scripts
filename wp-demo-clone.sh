f4052422932479f019dbc6d8e9701d6650d22e67
Varun Sridharan <varunsridharan23@gmail.com>
2021-03-18 06:54:57 +0530

    Update wp-demo-clone.sh

--- wp-demo-clone.sh
+++ wp-demo-clone.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+echo "Demo Instance Slug :"
+read
+DEMO_TEMPLATE="/var/www/template.sva.one"
+DEMO_SLUG=$REPLY
+DOMAIN_DOMAIN="${DEMO_SLUG}.sva.one"
+
+# Create Required Folders & Update Permissions
+mkdir -p "${BASE_DOC_ROOT}/${DOMAIN_NAME}/"
+sudo cp "${DEMO_TEMPLATE}/*" "${BASE_DOC_ROOT}/${DOMAIN_NAME}/"
+rm -rf "${BASE_DOC_ROOT}/${DOMAIN_NAME}/logs"
+mkdir -p "${BASE_DOC_ROOT}/${DOMAIN_NAME}/logs"
