8930287a79063bcda09a362bf8ebb44678ce774c
Varun Sridharan <varunsridharan23@gmail.com>
2021-03-18 12:50:27 +0530

    Updated

--- server/php/getdbname.php
+++ server/php/getdbname.php
@@ -2,4 +2,4 @@
 require '/var/www/global-files/php-prepend.php';
 require 'functions.php';
 $db_name = ( isset( $argv[1] ) ) ? $argv[1] : false;
-echo name_todb_name( $db_name );
+echo get_wpdemo_db_name( $db_name );
